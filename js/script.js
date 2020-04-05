066456bfae47d048ac68e9ad98c317542b19d89c
Maria Hennig <mhennig@eventbrite.com>
2020-04-05 23:53:35 +0100

    removed the snippet

--- js/script.js
+++ js/script.js
@@ -47,10 +47,6 @@ function getevents() {
             events.innerHTML= '<p>Sorry, there are no upcoming events.</p>'
         }
     })
-    
-    var snippet = document.getElementById('snippet');
-
-    snippet.innerText= "<script src='https://code.jquery.com/jquery-2.1.4.min.js'></script><script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.3/moment.js'></script><link rel='stylesheet' href='https://cdn.evbstatic.com/s3-build/28813-rc2018-08-29_16.04-33b338f/js/node_modules/eventbrite_design_system/css/eds.css'crossorigin='anonymous' /><link rel='stylesheet' href='https://cdn.rawgit.com/mhennigEB/Event-List-Widget/ed78fcb6/css/snippet.css'><div id='widgetbox' style='width:40%'><div id='orgId' style='display:none'>"+orgId+"</div><div id='events'></div><div class='eb-link'><i><a href='http://www.eventbrite.com' target=_blank>powered by Eventbrite</a></i></div></div><script src='https://cdn.rawgit.com/mhennigEB/Event-List-Widget/6185c70f/js/snippet-script.js' onload='getevents()'></script><script src='https://cdn.rawgit.com/mhennigEB/Event-List-Widget/5d9bdac8/js/event-card.js'>"
 }
 
 
