4a9626f31e7cbe088ee63947da2358e611f4f171
Maria Hennig <mariarhennig@gmail.com>
2020-04-05 23:41:25 +0100

    Updated to organization id endpoint

--- js/snippet-script.js
+++ js/snippet-script.js
@@ -1,43 +1,55 @@
 function getevents() {
+  const events = document.getElementById("events")
+  let orgId = document.getElementById('orgId').value
+  console.log(orgId)
+  if(orgId=="YOUR ORGANISER ID" || orgId ==""){
+      events.innerHTML ="<i>Please enter your organizer ID</i>"
+  } else {
+      events.innerHTML = "<i>Loading events, please stand by...</i>";
+  }
 
-    var org_id = document.getElementById('org_id').innerText;
-    var token = '3ZWVQM2G5TPBMVUR4D2N';
-    var events = document.getElementById("events");
-    
-    events.innerHTML = "<i>Loading events, please stand by...</i>";
-    
-    var call = 'https://www.eventbriteapi.com/v3/events/search/?organizer.id='+org_id+'&expand=venue&sort_by=date&token='+token;
-
-    $.get(call,
-
-    function(res) {
-        if(res.events.length) {
-            var s = "";
-            for(i=0;i<res.events.length;i++) {
-                var event = res.events[i];
-                var eventHtml = buildEventCard(event);
-                s += eventHtml;
-            }
-            events.innerHTML= s;
-        } else {
-            events.innerHTML= "<p>Sorry, there are no upcoming events.</p>";
-        }
-    });
+  const call = 'https://www.eventbriteapi.com/v3/organizations/'+orgId+'/events/?status=live&token='+token
+  
+  $.get(call,function(result){
+      let eventList = result.events
+      
+     if(eventList.length){
+          events.innerHTML = ''
+         for(i=0;i<eventList.length;i++){
+             let event = eventList[i]
+             console.log(event)
+             let location
+             let city
+             if(event.online_event){
+                  location = 'online'
+                  city = ''
+                  let eventCard = buildEventCard(event, location, city)
+                  events.innerHTML += eventCard
+              }else if(event.venue_id == null){
+                  location = ''
+                  city = ''
+                  let eventCard = buildEventCard(event, location, city)
+                  events.innerHTML += eventCard
+              }else{
+                  const venueCall = 'https://www.eventbriteapi.com/v3/venues/'+event.venue_id+'/?token='+token
+                  $.get(venueCall,function(res) {
+                      location = res.address.address_1
+                      city = res.address.city
+                      let eventCard = buildEventCard(event, location, city)
+                      events.innerHTML += eventCard
+                  });
+              }
+         }
+      } else {
+          events.innerHTML= '<p>Sorry, there are no upcoming events.</p>'
+      }
+  })
 }
 
-function buildEventCard(event) {
-    var img_url = event.logo.url || "https://upload.wikimedia.org/wikipedia/commons/1/12/Testbild.png";
-    
-    var location = event.venue.name || event.venue.address.localized_address_display;
-    if(location == event.venue.name){
-      var city = ", "+ event.venue.address.city;
-    } else {
-      var city = "";
-    }
-  
-    var url = event.url;
-  
-    return `
+function buildEventCard(event, location, city) {
+  const url = event.url;
+  const img_url = event.logo != null ? event.logo.url : 'https://upload.wikimedia.org/wikipedia/commons/1/12/Testbild.png'
+  return `
       <div class="eds-card eds-card--neutral" role="presentation">
         <section
           class="eds-l-pad-all-6 eds-media-card-content eds-media-card-content--grid eds-media-card-content--standard eds-media-card-content--fixed"
@@ -77,11 +89,11 @@ function buildEventCard(event) {
                       ${location} ${city}
                     </div>
                     <div class="eds-text-bs--fixed eds-text-color--grey-600 eds-l-mar-top-1" />
-                        <a href="${url}" target=_blank>
-                            <button class="eds-btn eds-btn--button eds-btn--fill btn-align-right" type="button">
-                            Get Tickets
-                            </button>
-                        </a>
+                      <a href="${url}" target=_blank>
+                        <button class="eds-btn eds-btn--button eds-btn--fill btn-align-right" type="button">
+                        Get Tickets
+                        </button>
+                      </a>
                   </div>
                 </div>
               </div>
@@ -89,6 +101,6 @@ function buildEventCard(event) {
           </main>
         </section>
       </div>
-    `;
-  }
+    `
+}
   
